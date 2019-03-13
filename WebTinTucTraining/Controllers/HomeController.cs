6073cc8a54a8d9dcee0a78b13efbf12f2a20297a
Le Hoang Trinh <hoangtrinh1610@gmail.com>
2019-03-13 21:49:51 +0700

    Initial project and demo mvc

---
+++ WebTinTucTraining/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using WebTinTucTraining.Models;
+
+namespace WebTinTucTraining.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Hello = "Xin chào các bạn";
+            ViewData["XinChao"] = "Xin chào nữa nè";
+
+            var model = new HelloModel();
+            model.LastName = "Trình";
+            model.LastName = "Lê Hoàng";
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebTinTucTraining/Models/HelloModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebTinTucTraining.Models
+{
+    public class HelloModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
