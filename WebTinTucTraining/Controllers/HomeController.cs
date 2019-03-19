6f97a416d29bd379672745bda5ded91ba4c1dd65
Le Hoang Trinh <hoangtrinh1610@gmail.com>
2019-03-19 22:07:35 +0700

    Adding template

--- WebTinTucTraining/Controllers/HomeController.cs
+++ WebTinTucTraining/Controllers/HomeController.cs
@@ -7,27 +7,6 @@ namespace WebTinTucTraining.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Hello = "Xin chào các bạn";
-            ViewData["XinChao"] = "Xin chào nữa nè";
-
-            var model = new HelloModel();
-            model.LastName = "Trình";
-            model.LastName = "Lê Hoàng";
-
-            return View(model);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
